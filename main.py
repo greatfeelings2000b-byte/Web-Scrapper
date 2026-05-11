940d83cb41bb0d0b7a085381d73a0971838c6a48
Muhammad Huzaifa Khan <greatfeelings2000b@gmail.com>
2026-05-11 13:40:45 -0700

    Extract book price stock and links from HTML

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-html=requests.get("https://www.mykhel.com/cricket/most-runs-in-odi-rs1/").text
-print(html)
+html=requests.get("https://books.toscrape.com/catalogue/category/books/science-fiction_16/index.html").text
+soup=BeautifulSoup(html,"lxml")
+book=soup.find('article', class_="product_pod")
+# div=section.find_all('div')
+# target_div=div[1]
+# elements=target_div.find('ol')
+# books=elements.find('li')
+price_and_stock=book.find('div', class_='product_price')
+price=price_and_stock.find('p', class_='price_color').text
+stock=price_and_stock.find('p', class_='instock availability').get_text(strip=True)
+link=book.h3.a['href']
+print(f'this book is {stock} and its price is {price} for further information click on this {link}')
