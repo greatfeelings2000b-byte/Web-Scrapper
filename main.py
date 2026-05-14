96007c07bba0af1858ea79fdcc28d810b9f966af
Muhammad Huzaifa Khan <greatfeelings2000b@gmail.com>
2026-05-14 15:33:15 -0700

    Add multi-book scraping with price filtering

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 html=requests.get("https://books.toscrape.com/catalogue/category/books/science-fiction_16/index.html").text
 soup=BeautifulSoup(html,"lxml")
-book=soup.find('article', class_="product_pod")
-# div=section.find_all('div')
-# target_div=div[1]
-# elements=target_div.find('ol')
-# books=elements.find('li')
-price_and_stock=book.find('div', class_='product_price')
-price=price_and_stock.find('p', class_='price_color').text
-stock=price_and_stock.find('p', class_='instock availability').get_text(strip=True)
-link=book.h3.a['href']
-print(f'this book is {stock} and its price is {price} for further information click on this {link}')
+books=soup.find_all('article', class_="product_pod")
+books_data=[]
+for b,book in enumerate(books):
+    price_and_stock=book.find('div', class_='product_price')
+    price=price_and_stock.find('p', class_='price_color').text
+    stock=price_and_stock.find('p', class_='instock availability').get_text(strip=True)
+    link=book.h3.a['href']
+    title=book.h3.a['title']
+    price_value=float(price[2:])
+    if price_value<=45.00:
+        books_info={"book_no"  :len(books_data)+1,
+                    "title"    :title,
+                    "stock"    :stock,
+                    "price": f"£{price_value}",
+                    "more_info":link}
+        books_data.append(books_info)
+    
+for book in books_data:
+    for key,value in book.items():
+        print(f"{key}: {value}")
+    print()
+        
